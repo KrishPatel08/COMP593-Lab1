5f30514af491f40d4981f02bd8e8636089e540c2
KrishPatel08 <KrishUmeshbhai.Patel@flemingcollege.ca>
2023-01-23 13:08:02 -0500

    Modified both files in the repo clone
    - Deleted a print() statement from script1.py
    - Added one print() statements to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # Print lines from Zen of Python
 print(f'Hii!')
-print(f'My name is Krish')
--- script2.py
+++ script2.py
@@ -1,4 +1,5 @@
 # This is goog programming advice
+print(f'My name is Krish.')
 print(f'How are you?')
 print(f'I am writing a script.')
 print(f'Example 1.')
